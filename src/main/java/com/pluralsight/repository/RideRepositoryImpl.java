c2414df1d52d975e52a28cb49b5ee81180e51d55
Clyde Mendonca <clyde.mendonca01@gmail.com>
2020-02-05 19:47:40 +0530

    PreparedStatement to insert row

--- src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
@@ -1,13 +1,15 @@
 package com.pluralsight.repository;
 
-import java.sql.ResultSet;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import com.pluralsight.model.Ride;
@@ -15,21 +17,44 @@ import com.pluralsight.repository.util.RideRowMapper;
 
 @Repository("rideRepository")
 public class RideRepositoryImpl implements RideRepository {
-	
+
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
-	
+
 	@Override
 	public List<Ride> getRides() {
-		List <Ride> rides = jdbcTemplate.query("SELECT * FROM ride", new RideRowMapper());
+		List<Ride> rides = jdbcTemplate.query("SELECT * FROM ride", new RideRowMapper());
 		return rides;
 	}
 
 	@Override
 	public Ride createRide(Ride ride) {
 		// TODO Auto-generated method stub
-		jdbcTemplate.update("INSERT INTO ride (name, duration) VALUES (?,?);", ride.getName(), ride.getDuration());
+//		jdbcTemplate.update("INSERT INTO ride (name, duration) VALUES (?,?);", ride.getName(), ride.getDuration());
+
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+		jdbcTemplate.update(new PreparedStatementCreator() {
+
+			@Override
+			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+				// TODO Auto-generated method stub
+				PreparedStatement ps = con.prepareStatement("INSERT INTO ride (name, duration) VALUES (?,?)",
+						new String[] { "id" });
+				ps.setString(1, ride.getName());
+				ps.setInt(2, ride.getDuration());
+				return ps;
+			}
+		}, keyHolder);
+
+		Number id = keyHolder.getKey();
+
+		return getRide(id.intValue());
+	}
+
+	private Ride getRide(int id) {
+		// TODO Auto-generated method stub
+		Ride ride = jdbcTemplate.queryForObject("SELECT * FROM ride WHERE id=?", new RideRowMapper(), id);
 		return ride;
 	}
-	
+
 }
